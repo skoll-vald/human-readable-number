3e4adc824d09c6ad69805d41fddf59eb35d5914b
skoll-vald <skollvald@gmail.com>
2023-01-31 17:17:36 +0300

    init: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function toReadable (number) {
-  
+const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+module.exports = function toReadable(number) {
+    if (number < 10) return ones[number];
+    if (number < 20) return teens[number - 10];
+    if (number < 100) return tens[Math.floor(number / 10)] + (number % 10 ? ' ' + ones[number % 10] : '');
+    if (number < 1000) return ones[Math.floor(number / 100)] + ' hundred' + (number % 100 ? ' ' + toReadable(number % 100) : '');
 }
